1853abf42b797a06a55a706ecd6642e89e35d6bd
Joseph Yuan <joeyuan19@gmail.com>
2013-10-16 02:52:02 +0000

    settings defined for local and server

--- PersonalSite/manage.py
+++ PersonalSite/manage.py
@@ -4,9 +4,10 @@ import sys
 import socket
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PersonalSite.production_settings")
-    
-    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PersonalSite.settings")
+    if socket.gethostname() == 'web425.webfaction.com':
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PersonalSite.production_settings")
+    else:
+    	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PersonalSite.settings")
 
     from django.core.management import execute_from_command_line
 
