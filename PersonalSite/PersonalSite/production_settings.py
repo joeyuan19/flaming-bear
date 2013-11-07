2c91f260609bf7bdf88f3bbf5842338a2e52f3f5
joeyuan19 <joe.yuan19@gmail.com>
2013-11-07 15:09:44 -0500

    settings updated

--- PersonalSite/PersonalSite/production_settings.py
+++ PersonalSite/PersonalSite/production_settings.py
@@ -24,7 +24,7 @@ except:
 
 ALLOWED_HOSTS = ['www.joeyuan.com','joeyuan.com']
 
-STATIC_URL = 'www.joeyuan.com/static/'
+STATIC_URL = ''
 STATICFILES_DIRS += (
 	'/home/joeyuan19/webapps/personal_site_django_app/flaming-bear/PersonalSite/static/',
 )
