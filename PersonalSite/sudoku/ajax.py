94b6b00015acfcb282f6097c7442ce5c4cddd04b
joeyuan19 <joe.yuan19@gmail.com>
2013-12-03 21:47:46 -0500

    pretty colors:

--- PersonalSite/sudoku/ajax.py
+++ PersonalSite/sudoku/ajax.py
@@ -1,6 +1,16 @@
 from dajaxice.decorators import dajaxice_register
 from django.utils import simplejson
 import sudoku.sudoku3 as s
+import re
+
+def smart_stringify(time):
+	return "%.8f" % time
+	time = str(time)
+	time = re.sub(r'0+$', '', time)
+	time = re.sub(r'([\d]{4})\d+(e-\d+)$',r'\g<1>\g<2>',time)
+	return time
+
+
 
 @dajaxice_register(name='sudoku.solve')
 def solve(request,puzzle=None):
@@ -10,6 +20,8 @@ def solve(request,puzzle=None):
 	else:
 		time,puzzle = s.solve_puzzle(puzzle)
 		solved = True
+	if time is not None:
+		time = smart_stringify(time)
 	return simplejson.dumps({'solved':solved,'puzzle':puzzle,'time':time})
 
 
