66c3882282a049a4c20cac466760b0005612213f
Joe Yuan <Joe.Yuan19@gmail.com>
2014-04-08 15:52:47 -0400

    django res test

---
+++ PersonalSite/content/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from content.models import Resume, ResumeCategory
+
+class ResumeAdmin(admin.ModelAdmin):
+	list_display = ('title')
+
+class ResumeCategoryAdmin(admin.ModelAdmin):
+	list_display = ('title')
+
+admin.site.register(Resume,ResumeAdmin)
+admin.site.register(ResumeCategory,ResumeCategoryAdmin)
+
+
+
