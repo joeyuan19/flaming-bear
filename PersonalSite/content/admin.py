4f53cce31187ec1f564a712bcba287785df81b92
Joe Yuan <Joe.Yuan19@gmail.com>
2014-04-08 15:55:14 -0400

    admin functions

--- PersonalSite/content/admin.py
+++ PersonalSite/content/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from content.models import Resume, ResumeCategory
 
 class ResumeAdmin(admin.ModelAdmin):
-	list_display = ('title')
+	list_display = ('title',)
 
 class ResumeCategoryAdmin(admin.ModelAdmin):
-	list_display = ('title')
+	list_display = ('title',)
 
 admin.site.register(Resume,ResumeAdmin)
 admin.site.register(ResumeCategory,ResumeCategoryAdmin)
