a94aee2f8718ce8f21c19d1bd079496c5895fadc
Joe Yuan <Joe.Yuan19@gmail.com>
2014-04-08 16:12:45 -0400

    yup

--- PersonalSite/content/models.py
+++ PersonalSite/content/models.py
@@ -9,7 +9,16 @@ class Project(models.Model):
     preview = models.ImageField(upload_to="img/project_previews/",blank=True,null=True,default=None)
     rel_date    = models.CharField(max_length=128,blank=True,null=True,default="")
     url = models.URLField(max_length=128,blank=True,null=True,default="")
-    modified = models.DateField(blank=True)
+    modified = models.DateField(editable=False,blank=True)
+
+    def __repr__(self):
+        return "<Project tile:"+self.title+">"
+
+    def __unicode__(self):
+        return self.__repr__()
+
+    def __str__(self):
+        return self.__repr__()
 
     def save(self,*args,**kwargs):
         self.modified = timezone.now()
@@ -18,6 +27,16 @@ class Project(models.Model):
 class ProjectCategory(models.Model):
     title = models.CharField(max_length=64)
     entries = models.ManyToManyField(Project)
+    
+    def __repr__(self):
+        return "<Project Category tile:"+self.title+">"
+
+    def __unicode__(self):
+        return self.__repr__()
+
+    def __str__(self):
+        return self.__repr__()
+
 
 class Contact(models.Model):
     pass
@@ -26,7 +45,16 @@ class Resume(models.Model):
     title = models.CharField(max_length=128,blank=True,null=True,default="")
     description = models.TextField(blank=True,null=True,default="")
     relevent_dates = models.CharField(max_length=128,blank=True,null=True,default="")
-    modified = models.DateTimeField(blank=True)
+    modified = models.DateTimeField(editable=False,blank=True)
+    
+    def __repr__(self):
+        return "<Resume tile:"+self.title+">"
+
+    def __unicode__(self):
+        return self.__repr__()
+
+    def __str__(self):
+        return self.__repr__()
 
     def save(self,*args,**kwargs):
         self.modified = timezone.now()
@@ -35,6 +63,16 @@ class Resume(models.Model):
 class ResumeCategory(models.Model):
     title = models.CharField(max_length=64)
     entries = models.ManyToManyField(Resume)
+    
+    def __repr__(self):
+        return "<Resume Category tile:"+self.title+">"
+
+    def __unicode__(self):
+        return self.__repr__()
+
+    def __str__(self):
+        return self.__repr__()
+
 
 class Friend(models.Model):
     name = models.CharField(max_length=64,blank=True,null=True,default="")
@@ -42,6 +80,15 @@ class Friend(models.Model):
     Description = models.CharField(max_length=256,blank=True,null=True,default="")
     url = models.URLField(max_length=128,blank=True,null=True,default="")
     modified = models.DateTimeField(editable=False,blank=True)
+    
+    def __repr__(self):
+        return "<Friend name:"+self.name+">"
+
+    def __unicode__(self):
+        return self.__repr__()
+
+    def __str__(self):
+        return self.__repr__()
 
     def save(self,*args,**kwargs):
         self.modified = timezone.now()
