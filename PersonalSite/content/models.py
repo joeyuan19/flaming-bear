f3bc94e8ea45237872e0d439b077220b46dd2d8d
Joe Yuan <Joe.Yuan19@gmail.com>
2014-04-09 17:51:27 -0400

    models updated

--- PersonalSite/content/models.py
+++ PersonalSite/content/models.py
@@ -7,9 +7,10 @@ class Project(models.Model):
     title   = models.CharField(max_length=128,blank=True,null=True,default="")
     body    = models.TextField(blank=True,null=True,default="")
     preview = models.ImageField(upload_to="img/project_previews/",blank=True,null=True,default=None)
-    rel_date    = models.CharField(max_length=128,blank=True,null=True,default="")
+    rel_date = models.CharField(max_length=128,blank=True,null=True,default="")
     url = models.URLField(max_length=128,blank=True,null=True,default="")
     modified = models.DateField(editable=False,blank=True)
+    org_key = models.IntegerField(blank=True,default=0)
 
     def __repr__(self):
         return "<Project title:"+self.title+">"
@@ -27,6 +28,8 @@ class Project(models.Model):
 class ProjectCategory(models.Model):
     title = models.CharField(max_length=64)
     entries = models.ManyToManyField(Project,blank=True)
+    org_key = models.IntegerField(blank=True,default=0)
+
     
     def __repr__(self):
         return "<Project Category title:"+self.title+">"
@@ -65,6 +68,7 @@ class Resume(models.Model):
 class ResumeCategory(models.Model):
     title = models.CharField(max_length=64)
     entries = models.ManyToManyField(Resume,blank=True)
+    org_key = models.IntegerField(blank=True,default=0)
     
     def list_entries(self):
         return "\n".join([i.__repr__() for i in self.entries.all()])
@@ -91,6 +95,7 @@ class Friend(models.Model):
     Description = models.CharField(max_length=256,blank=True,null=True,default="")
     url = models.URLField(max_length=128,blank=True,null=True,default="")
     modified = models.DateTimeField(editable=False,blank=True)
+    org_key = models.IntegerField(blank=True,default=0)
     
     def __repr__(self):
         return "<Friend name:"+self.name+">"
